f04b4992fec9e1a375839f8b91dd6929721258e3
brtsrm <bsarmis@outlook.com>
2022-08-07 00:18:16 +0300

    react egitim ilk işlem

---
+++ my-app/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+function hello() {
+    return "Merhaba"
+}
+class App2 extends React.Component {
+    render() {
+        return hello()
+    }
+}
+
+root.render(
+    <App2 />
+)
