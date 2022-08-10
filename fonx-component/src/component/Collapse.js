9f692f33b2bb06605cc68d747797f9e4f8ea8127
brtsrm <bsarmis@outlook.com>
2022-08-10 13:53:35 +0300

    setState kullanımı

--- fonx-component/src/component/Collapse.js
+++ fonx-component/src/component/Collapse.js
@@ -2,16 +2,61 @@ import React from 'react'
 
 
 class Collapse extends React.Component {
+
+    constructor() {
+
+        super();
+        this.state = {
+            showContent: false,
+
+        }
+
+
+        /* 1. Yöntem **** 
+        this.showMore = () =>  {
+            this.setState({
+                showContent: true
+            })
+        } */
+        /* 2. Yöntem ****
+            this.showMore = this.showMore.bind(this)
+        */
+
+    }
+
+    /*  2. Yöntem ****
+        showMore(){
+            this.setState({
+                showContent : true
+            })
+        } 
+    */
+
+
+    /* 3. Yöntem **** */
+    showMore = () => {
+        this.setState({
+            showContent: !this.state.showContent,
+        })
+    }
+
+
     render() {
+
         return (
             <div>
-                <button className="btn btn-primary w-100" type="button" data-toggle="collapse" data-target={"#contendId" + this.props.itemId} aria-expanded="false"
-                    aria-controls="contentId">
+                <button className="btn btn-primary w-100" onClick={this.showMore}>
                     Göster
                 </button>
-                <div className="collapse show" id={"contendId" + this.props.itemId}>
-                    {this.props.children}
-                </div>
+
+                {
+                    this.state.showContent ? (
+                        <div className="collapse show" id={"contendId" + this.props.itemId}>
+                            {this.props.children}
+                        </div>
+                    ) : null
+                }
+
             </div>
         )
     }
